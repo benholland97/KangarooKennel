a16beb8446a9e6799d5a445e686a7fe8b90c6c0c
benholland97 <35569604+benholland97@users.noreply.github.com>
2018-03-01 22:51:45 +0000

    Add files via upload

---
+++ SensorNode/KennelMain.cpp
@@ -0,0 +1,27 @@
+#include "Configuration.hpp"
+#include "MyXbee.hpp"
+
+MyXbee *xbee;
+char receivedData[ZB_PACKET_SIZE];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setting up");
+  delay(5000);
+  xbee = new MyXbee();
+  xbee->setPayload("This is");
+}
+
+void loop() {
+  Serial.flush();
+//  xbee->sendMessage();
+  delay(2000);
+
+ 
+  if (xbee->receiveMessage()) {
+    xbee->getMessage(receivedData);
+    Serial.println(receivedData);
+  }
+
+}
+
---
+++ SensorNode/MyXbee.hpp
@@ -0,0 +1,34 @@
+
+#include "Configuration.hpp"
+
+class MyXbee {
+public:
+
+  bool sendMessage();
+
+  bool receiveMessage();
+
+  void setPayload(String msg);
+
+  void getMessage(char* data);
+
+  MyXbee();
+
+  ~MyXbee();
+
+private:
+  SoftwareSerial *mySerial;
+  XBee xbee;
+  String payloadVal;
+  XBeeAddress64 destAddr;
+  ZBTxRequest txReq;
+  ZBTxStatusResponse txStatus;
+
+  XBeeResponse rxResp;
+  ZBRxResponse rxData;
+  ModemStatusResponse msr;
+  byte checksum;
+  int packetLength;
+  char rxMessageTemp[ZB_PACKET_SIZE];
+  char rxMessage[ZB_PACKET_SIZE];
+};
