d09674d797ca73acf494e6bf03b65ba052c75892
Sentricity <38910607+Sentricity@users.noreply.github.com>
2018-05-02 15:13:01 +0100

    Updated Navbar

--- FinalWebsite/KennelFinal/index.php
+++ FinalWebsite/KennelFinal/index.php
@@ -45,14 +45,14 @@
           <ul class="sidenav-second-level collapse" id="collapseComponents">
               <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
                 <a class="nav-link" href="charts.html">
-                  <i class="fa fa-fw fa-table"></i>
+                  <i class="fa fa-fw fa-area-chart"></i>
                   <span class="nav-link-text">Graphical Analysis</span>
                 </a>
               </li>
             </li>
               <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
                 <a class="nav-link" href="tables.html">
-                  <i class="fa fa-fw fa-area-chart"></i>
+                 <i class="fa fa-fw fa-table"></i>
                   <span class="nav-link-text">Index Tables</span>
                 </a>
               </li>
