6d75c1012eed269732f6416b051bf8bfdb72ab5e
benholland97 <35569604+benholland97@users.noreply.github.com>
2018-05-04 12:09:58 +0100

    Add files via upload

--- FinalWebsite/KennelFinal/index.js
+++ FinalWebsite/KennelFinal/index.js
@@ -1,19 +1,28 @@
 var numKennels;
 
+
+//Function called once initial page load
 $(document).ready(function() {
+	//get the starting number of kennels in the network
 	getNumKennels();
+	//set the table of kennels equal to the starting number of kennels in network
 	setTable();
+	//fill values of each kennel entry in table
 	updateTable();
-	// setInterval(setTable, 5000);
+	//refresh page every 5 seconds - to provide 'real-time' response
 	setInterval(updateTable, 5000);
 });
 
 
+//function to fill table with entry for each kennel in network
 function setTable() {
+	//ensure number of kennels always updated to most recent value 
 	getNumKennels();
 	var $table = $('#kennel_table_body');
 	var $html = '';
+	//loop through all kennels in network
 	for (i=1; i < numKennels+1; i++) {
+			//formulate and concatenate html strings representing table entry data for each kennel
 			$html+= "<tr>";
 			$html+= 	"<td>"+i+"</td>";
 			$html+= 	"<td id='pres"+i+"'>-</td>";
@@ -22,30 +31,41 @@ function setTable() {
 			$html+= "</tr>";
 	}
 	console.log($html);
+	//replace old table code with newly formed code
 	$table.html($html);
 }
 
 
-
+//function to update values of all table entries
 function updateTable() {
+	//ensure most recent version of table present 
 	setTable();
-	
+	//iterate through all kennels
 	for (let i=1; i< numKennels+1; i++) {
 		console.log("iteration:"+i);
 		(function(index) {
+			
+			//set placeholder variables equal to corresponding loop 
 			var $pres = '#pres'+i;
 			var $temp = '#temp'+i;
 			var $humid = '#humid'+i;
 			var $presHtml = '';
-
+			//asynchronous request for database values for kennel ref passed
 			$.ajax({
 				url: 'load-data.php',
 				type: 'post',
 				dataType: 'json',
-				data: { Mode: 'kenData', Kennel: i },			//Send params to get different data 
+				data: { Mode: 'kenData', Kennel: i },			//send loop num representing kennel num
 				success: function(data) {
 					console.log(data);
-					//conditional formatting data 
+					
+					//test for value of 4th column of returned data representing presence 
+					if (parseInt(data[4]) == 0) {
+							$presHtml = "No";
+					} else if (parseInt(data[4]) == 1) {
+							$presHtml = "Yes";
+					}
+					//set value of table cells equal to returned data 
 					$($pres).html($presHtml);
 					$($temp).html(data[2]);
 					$($humid).html(data[3]);
@@ -53,14 +73,8 @@ function updateTable() {
 					//only conditional format if dog present
 					if (parseFloat(data[4]) == 1) {
 						console.log("dog present");
-						if (parseInt(data[4]) == 0) {
-							$presHtml = "No";
-						} else if (parseInt(data[4]) == 1) {
-							$presHtml = "Yes";
-						}
 
-						//temperature conditional formatting 
-						
+						//temperature conditional formatting 						
 						var temp = parseFloat(data[2]); 
 						
 						if (temp >= 27) {
@@ -120,7 +134,7 @@ function updateTable() {
 
 
 
-
+//function to return the number of kennels in the network 
 function getNumKennels() {
 	$.ajax({
 		url: 'load-data.php',
